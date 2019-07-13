900a69a2e7cb023a02922a3fc146a2a2eccf6105
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2019-07-12 23:09:25 -0700

    media query tests

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const about = {template:
   `
     <div class="templates">
       <h1>About Me.</h1>
-      <p>I am Gonzaga Univeristy graduate with a degree in marketing and just finished a coding bootcamp at PDX Code Guild.</p>
-      <p>My current objective is to find a postion within software engineering to develop my skills. I am quick to pick up new information and able to work well by myself and with others.</p>
+      <p>I have a BBA with a concentration in Marketing from Gonzaga University. I conducted digital marketing campaigns at the Unity Multicultural Education Center and more recently at KBOO radio station. Recently, I finished a coding bootcamp course from PDX Code Guild and received a certificate in Python Based Web Development.</p>
+      <p>My current objective is to find a position within software engineering to develop my skills. I am furthering my coding knowledge post bootcamp into other JavaScript libraries. I can pick up new information quickly and be efficient in it.</p>
 
       <p>I can work with</p>
         <div class="icons">
@@ -30,22 +30,19 @@ const projects = {template:
     <ul>
       <li>
         <div class="description">
-          <p>AmbitioUs is my capstone project for the PDX Code Guild bootcamp. The
-          app is a platform for people to share goals with other people who want
-          to achieve grand goals.</p>
-          <p>The app itself is still underdevelopement but it was built with Vue,
-          Python, and Django.</p>
+          <p>AmbitioUs is my capstone project from the PDX Code Guild bootcamp. The
+          app is a platform where users can design a long-term goal with subsequent milestones to ensure a gradual progression towards the goal. The app is still under development and I will continue to work on it as I progress in my skills.</p>
+          <p>AmbitioUs was built using Django and REST APIs. The templates are using a combination of Django and Vue, for reactivity.</p>
         </div>
         <div class="visuals">
           <img src="img/ambitious.gif">
         </div>
       </li>
+      <hr>
       <li>
         <div class="description">
-          <p>Con Ganas is a clothing line that promotes Latinx people to be
-          proud of who they are and push through the adversity of today's world.
-          </p>
-          <p>I started Con Ganas while in an entrepenuership class at Gonzaga.</p>
+          <p>Con Ganas is a clothing line that I started while in an entrepenuership class at Gonzaga. It represents the pride I have as a Latino man and the pride Latinx people showcase on a daily basis.</p>
+          <p>Con Ganas means “to do something full heartedly”.</p>
           <p>The website is currently unavailable but I do plan on bringing this
           idea back to life.</p>
         </div>
@@ -62,15 +59,19 @@ const goals = {template:
   `
     <div class="goals">
       <h1>Current Goals.</h1>
-      <p>I love to challenge myself to try new things. The mindset of growth is one that I constantly strive to be in. I find a sense of discovery to be very important. That being said, I do new activities no to the detriment of myself or my previous goals. When there is time and available energy, I try to grow.</p>
+      <p>I love to challenge myself to try new things. The mindset of growth is one that I constantly strive to be in.</p>
       <ul>
+      <li>
+        <h2>Coding</h2>
+        <p>I have continued my education post bootcamp focusing on becoming a confident full stack developer. I have studied Node, MongoDb, and VueRouter on my own. I am moving towards working on more projects with a VENM stack. I will also start to study React to become better verse in current JavaScript frameworks.</p>
+      </li>
         <li>
           <h2>Running</h2>
           <p>I discovered the joy of running at the beginning of this year. I finished a half marathon in late March and just recently started training for a full marathon. I will be running the Portland Marathon in October 6th!</p>
         </li>
         <li>
           <h2>Music</h2>
-          <p>After graduating from Gonzaga I found myself not practicing my saxophone as much as before. After 11 years of playing the saxophone, I owe it to myself to at least play once in a jam session somewhere around town. If you play an instrument and would like to jam together, let me know.</p>
+          <p>I want to play at a local jam session in Portland, OR. I have been playing saxophone for around 12 years and want to play in new scenes.</p>
         </li>
       </ul>
     </div>
